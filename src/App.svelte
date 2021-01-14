a2e48f13b3039f766711583cd1beb0979158304c
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2021-01-14 11:22:58 -0500

    started, that's about it.

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,43 @@
 <script lang="ts">
 
-	import Tab, {Icon, Label} from '@smui/tab';
-  	import TabBar from '@smui/tab-bar';
-	
-	//import Random from "./behavior/random";
-	import About from "./components/About.svelte";
-	import PoolEditor from './components/PoolEditor.svelte';
-	import Lobby from './components/Lobby.svelte';
-	import User from './components/User.svelte';
+	import { FirebaseApp, User, Doc, Collection } from "sveltefire";
+	import firebase from "firebase/app";
+	import "firebase/firestore";
+	import "firebase/auth";
 
-
-	var user=null;
-
-	let active = "Profile";
+	let firebaseConfig = {
+		apiKey: "AIzaSyBmskNn5aeKBE0q-_YBEYQm9J0-_eZrXQ0",
+		authDomain: "among-friends-inhouses.firebaseapp.com",
+		projectId: "among-friends-inhouses",
+		storageBucket: "among-friends-inhouses.appspot.com",
+		messagingSenderId: "2420558964",
+		appId: "1:2420558964:web:02fc063f14b8fd862a9521"
+	};
+	firebase.initializeApp(firebaseConfig);
 </script>
-
-<TabBar tabs={["Profile", "Champion Pool", "Game", "About"]} let:tab bind:active>
-	<Tab {tab}>
-		<Label>{tab}</Label>
-	</Tab>
-</TabBar>
 <main>
-	{#if active==="Profile"}
-		<User bind:user={user}/>
-	{:else if active==="Champion Pool"}
-		<PoolEditor/>
-	{:else if active==="Game"}
-		<Lobby/>
-	{:else if active==="About"}
-		<About/>
-	{/if}
-	
-</main>
+<FirebaseApp {firebase}>
+	<User let:user let:auth>
+		Howdy 😀! User
+		<em>{user.displayName}</em>
+		<button on:click={() => auth.signOut()}>Sign Out</button>
 
+      	<div slot="signed-out">
+			<button on:click={() => auth.signInAnonymously()}>
+			Sign In Anonymously
+			</button>
+		</div>
+		<Doc path={'champ_pools/'+user.uid} let:data={pool} let:ref={poolRef}>
+			<h1>{pool.name}</h1>
+			<ul>
+				{#each pool.champions as champion}
+					<li>{champion.name}</li>
+				{/each}
+			</ul>
+		</Doc>
+	</User>
+</FirebaseApp>
+</main>
 <style>
 	main {
 		padding: 32px;
